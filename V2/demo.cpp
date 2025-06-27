371811b5194253071563e409c5e1f346a2a30dd0
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-06-28 02:07:12 +0530

    Learning basic concepts

--- V2/demo.cpp
+++ V2/demo.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+  cout << "Hi" << endl;
+
+  int x;
+  cin >> x;
+  cout << "Val :" << x << endl;
+  string s1, s2;
+  cin >> s1;
+  cin >> s2;
+  cout << "String is :" << s1 << s2 << endl;
+  cin.ignore();
+
+  string str;
+  getline(cin, str);
+  cout << str << endl;
   cout << "Hi";
   return 0;
 }
