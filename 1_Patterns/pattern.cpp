8c542c3258f021e6b11e2ffeed2cf3511d687819
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-07-01 00:49:53 +0530

    Pattern 12-22

--- 1_Patterns/pattern.cpp
+++ 1_Patterns/pattern.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
@@ -162,7 +163,179 @@ void pattern11(int n) {
     cout << endl;
   }
 }
+void pattern12(int n) {
 
+  for (int i = 1; i <= n; i++) {
+
+    for (int j = 1; j <= i; j++) {
+      cout << j;
+    }
+    for (int j = 1; j <= 2 * n - 2 * i; j++) {
+      cout << " ";
+    }
+    for (int j = i; j >= 1; j--) {
+      cout << j;
+    }
+
+    cout << endl;
+  }
+}
+
+void pattern13(int n) {
+
+  int number = 1;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << number << " ";
+      number++;
+    }
+
+    cout << endl;
+  }
+}
+void pattern14(int n) {
+
+  for (int i = 0; i < n; i++) {
+    // char ch = 'A';
+    // for (int j = 1; j <= i + 1; j++) {
+    //   cout << ch << " ";
+    //   ch++;
+    // }
+
+    // OR
+    for (char j = 'A'; j <= 'A' + i; j++) {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+}
+void pattern15(int n) {
+  for (int i = 0; i < n; i++) {
+    for (char j = 'A'; j <= 'A' + (n - i - 1); j++) {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+}
+void pattern16(int n) {
+  for (int i = 0; i < n; i++) {
+    char ch = 'A' + i;
+    for (char j = 0; j <= i; j++) {
+      cout << ch << " ";
+    }
+    cout << endl;
+  }
+}
+void pattern17(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      cout << " ";
+    }
+    char ch = 'A';
+    for (int j = 0; j < 2 * i + 1; j++) {
+      cout << ch;
+      int breakpoint = (2 * i + 1) / 2;
+      if (j < breakpoint) {
+        ch++;
+      } else {
+        ch--;
+      }
+    }
+
+    cout << endl;
+  }
+}
+void pattern18(int n) {
+  for (int i = 0; i < n; i++) {
+    char ch = 'A' + n - i - 1;
+    for (int j = 0; j <= i; j++) {
+      cout << ch;
+      ch++;
+    }
+    cout << endl;
+  }
+}
+void pattern19(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - i; j++) {
+      cout << "*";
+    }
+    for (int j = 0; j < 2 * i; j++) {
+      cout << " ";
+    }
+    for (int j = 0; j < n - i; j++) {
+      cout << "*";
+    }
+
+    cout << endl;
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i + 1; j++) {
+      cout << "*";
+    }
+    for (int j = 0; j < 2 * n - 2 * (i + 1); j++) {
+      cout << " ";
+    }
+    for (int j = 0; j < i + 1; j++) {
+      cout << "*";
+    }
+
+    cout << endl;
+  }
+}
+
+void pattern20(int n) {
+  int space = 2 * n - 2;
+  for (int i = 1; i <= 2 * n - 1; i++) {
+    int stars = i;
+    if (i > n)
+      stars = 2 * n - i;
+    for (int j = 1; j <= stars; j++) {
+      cout << "*";
+    }
+
+    for (int j = 1; j <= space; j++) {
+      cout << " ";
+    }
+
+    for (int j = 1; j <= stars; j++) {
+      cout << "*";
+    }
+    cout << endl;
+    if (i < n)
+      space -= 2;
+    else
+      space += 2;
+  }
+}
+
+void pattern21(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+        cout << "*";
+      } else {
+        cout << " ";
+      }
+    }
+    cout << endl;
+  }
+}
+
+void pattern22(int n) {
+  for (int i = 0; i < 2 * n - 1; i++) {
+    for (int j = 0; j < 2 * n - 1; j++) {
+      int topDistance = i;
+      int leftDistance = j;
+      int rightDistance = (2 * n - 2) - j;
+      int bottomDistance = (2 * n - 2) - i;
+      cout << n - min(min(topDistance, leftDistance),
+                      min(rightDistance, bottomDistance));
+    }
+    cout << endl;
+  }
+}
 int main() {
   int n;
   cin >> n;
@@ -175,7 +348,18 @@ int main() {
   // pattern7(n);
   // pattern8(n);
   // pattern9(n);
-  pattern10(n);
-  pattern11(n);
+  // pattern10(n);
+  // pattern11(n);
+  // pattern12(n);
+  // pattern13(n);
+  // pattern14(n);
+  // pattern15(n);
+  // pattern16(n);
+  // pattern17(n);
+  // pattern18(n);
+  // pattern19(n);
+  // pattern20(n);
+  // pattern21(n);
+  pattern22(n);
   return 0;
 }
