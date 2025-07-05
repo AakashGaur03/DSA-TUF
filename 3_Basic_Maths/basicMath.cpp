8914676d3ccc9c97b157b717482cbe52e47957c9
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-07-06 02:23:59 +0530

    Basic maths Complete
    Prime Number
    GCD HCF
    LCM
    Euclidean Algorithm

--- 3_Basic_Maths/basicMath.cpp
+++ 3_Basic_Maths/basicMath.cpp
@@ -103,6 +103,125 @@ void printAllDivisors(int num) {
   }
 }
 
+bool primeNumber(int num) {
+  // prime Number is
+  // That has exactly two factors 1 and Itself
+
+  // TC O(n)
+  // int cnt = 0;
+  // for (int i = 0; i <= num; i++) {
+  //   if (num % i == 0) {
+  //     cnt++;
+  //   }
+  // }
+  // if (cnt == 2) {
+  //   return true;
+  // } else
+  //   return false;
+
+  // TC O(sqrt n)
+  int cnt = 0;
+  for (int i = 1; i * i <= num; i++) {
+    if (num % i == 0) {
+      if (i == num / i) {
+        cnt++;
+      } else {
+        cnt += 2;
+      }
+    }
+  }
+  return cnt == 2;
+}
+
+int GCD_HCF(int num1, int num2) {
+  // Greatest Comman Divisor or Highest Common Factor
+  // Ex N1 = 9 , N2 = 12
+  // Factors of 9 : 1,3,9
+  // Factors of 12 : 1,2,3,4,6,12
+  // GCD or HCF is 3
+  // int smallNum = -1;
+  // if (num1 < num2) {
+  //   smallNum = num1;
+  // } else {
+  //   smallNum = num2;
+  // }
+  // int smallNum = min(num1, num2);
+  // Brute Force
+  // TC O(minimum of num1 and num2) O(min(n1,n2))
+  int smallNum = num1 < num2 ? num1 : num2;
+  int result = -1;
+  // for (int i = 1; i <= smallNum; i++) {
+  //   if (num1 % i == 0 && num2 % i == 0) {
+  //     result = i;
+  //   }
+  // }
+  // In Most cases we can do
+  for (int i = smallNum; i >= 1; i--) {
+    if (num1 % i == 0 && num2 % i == 0) {
+      result = i;
+      break;
+    }
+  }
+  return result;
+}
+
+int optimizedGCD_HCF(int num1, int num2) {
+  // Euclidean Algorithm
+  // gcd of n1, n2
+  // gcd(n1,n2) = gcd(n1-n2,n2) where n1>n2
+  // gcd(a,b) = gcd(a-b,b) where a>b
+  // Lets take an example
+  // n1=20 n2 =15
+  // gcd(20, 15) = gcd(20-15,15)=gcd(5,15)
+  // gcd(5,15) =now we will find gcd(15,5)
+  // gcd(15,5) = gc(15-5,5) => gcd(10,5)
+  // gcd(10,5) = gc(10-5,5) => gcd(5,5)
+  // gcd(5,5) = gc(5-5,5) => gcd(0,5)
+  // When one number becomes zero then second number is gcd
+
+  // in easy way we can also say to save more dividing steps
+  // gcd(a,b) = gcd(a%b,b);  where a>b;
+  // Till one number becomes zero
+
+  // TC O(log φ(min(n1,n2)) )
+  // φ can be any number
+  int result;
+  while (num1 > 0 && num2 > 0) {
+    if (num1 > num2)
+      num1 = num1 % num2;
+    else
+      num2 = num2 % num1;
+  }
+  if (num1 == 0)
+    return num2;
+  return num1;
+}
+
+int LCM(int num1, int num2) {
+  int maxNum = (num1 > num2) ? num1 : num2;
+
+  for (int i = maxNum;; i++) {
+    if (i % num1 == 0 && i % num2 == 0) {
+      return i;
+    }
+  }
+}
+
+int GCD(int num1, int num2) {
+  while (num1 > 0 && num2 > 0) {
+    if (num1 > num2)
+      num1 = num1 % num2;
+    else
+      num2 = num2 % num1;
+  }
+  return (num1 == 0) ? num2 : num1;
+}
+
+int LCM_With_GCD(int num1, int num2) {
+  // Optimized
+  return (num1 * num2) / GCD(num1, num2);
+}
+
 int main() {
   cout << "Hi Basic Maths" << endl;
 
@@ -131,7 +250,9 @@ int main() {
   // cout << armStrongNumber(num);
   // cout << "Armstrong or Not : " << armStrongNumber(num);
 
+  // cout << endl;
+  // printAllDivisors(num);
   cout << endl;
-  printAllDivisors(num);
+  cout << "Is Prime Number :" << primeNumber(num);
   return 0;
 }
