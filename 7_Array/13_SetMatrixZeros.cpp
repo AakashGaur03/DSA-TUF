e1edc6d61bf5dca7f5f7ecb85c6bcda5c2c0ecc6
Aakash Gaur <aakash.sunrise23@gmail.com>
2025-07-28 16:05:12 +0530

    Set Matrix Zero

--- 7_Array/13_SetMatrixZeros.cpp
+++ 7_Array/13_SetMatrixZeros.cpp
@@ -73,6 +73,70 @@ void betterSetMarkZero(vector<vector<int>> &arr) {
     }
   }
 }
+
+void optimalSetMarkZero(vector<vector<int>> &arr) {
+  // In the better approach we used extra space (rowSize, colSize arrays).
+  // In this optimal approach, we use the matrix itself to store the markers.
+  // We use first row and first column as marker arrays.
+  // Special care is taken for arr[0][0], which is common to both row and col.
+  // So, we use a separate variable `col0` to track if the first column needs to
+  // be zeroed.
+
+  // TC O(NXM) + O(NXM)
+  // SC O(1)
+  int n = arr.size();    // row
+  int m = arr[0].size(); // col
+
+  // vector<int> rowSize(n, 0);     -> matrix[0][...]
+  // vector<int> colSize(m, 0);     -> matrix[...][0]
+
+  // Step 2: Traverse the matrix and mark the corresponding row and column
+  // if a cell contains 0
+  int col0 = 1;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (arr[i][j] == 0) {
+        // mark the ith row
+        arr[i][0] = 0; // mark current row
+        // mark the jth col
+        if (j != 0) {
+
+          arr[0][j] = 0; // mark current column
+        } else
+          col0 = 0; // special case for column 0
+      }
+    }
+  }
+  // Marking is done in above loop
+
+  // Step 2: Use markers to set cells to zero (skip first row and column)
+  // Now we will iterate without inplace row and col
+  for (int i = 1; i < n; i++) {
+    for (int j = 1; j < m; j++) {
+      if (arr[i][j] != 0) {
+        // Check for col and row
+        if (arr[0][j] == 0 || arr[i][0] == 0) {
+          arr[i][j] = 0;
+        }
+      }
+    }
+  }
+  // Step 3: Handle first row separately
+  // Check first row
+  if (arr[0][0] == 0) {
+    for (int j = 0; j < m; j++) {
+      arr[0][j] = 0;
+    }
+  }
+  // Step 4: Handle first column separately (using col0 flag)
+  // Check for col
+  if (col0 == 0) {
+    for (int i = 0; i < n; i++) {
+      arr[i][0] = 0;
+    }
+  }
+}
+
 int main() {
   cout << "Array 13" << endl;
 
@@ -95,7 +159,8 @@ int main() {
       {1, 1, 1, 1},
   };
   //   bruteSetMarkZero(arr);
-  betterSetMarkZero(arr);
+  // betterSetMarkZero(arr);
+  optimalSetMarkZero(arr);
 
   for (auto row : arr) {
     for (auto val : row) {
