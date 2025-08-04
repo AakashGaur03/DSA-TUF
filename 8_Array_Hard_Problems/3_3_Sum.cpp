d0e99dd8a7e5c0304b113ebe0b7758b2a8d971e5
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-08-05 01:25:05 +0530

    # Sum Problem Optimized approach

--- 8_Array_Hard_Problems/3_3_Sum.cpp
+++ 8_Array_Hard_Problems/3_3_Sum.cpp
@@ -50,6 +50,61 @@ vector<vector<int>> better3Sum(vector<int> arr, int size) {
   return ans;
 }
 
+vector<vector<int>> optimal3Sum(vector<int> arr, int size) {
+
+  // TC O(nlogn) (sort) O(N) for loop ~O(N) for while loop
+  // TC O(nlogn + N^2)
+  // SC O(M) (for returing answer)
+
+  // Sort the Array
+
+  // Two Pointer Approach
+
+  // we will keep i at start j at start+1 and k at last
+  //  we will sum them up if i+j+k gives less tehn zero we will do j++ is
+  //  greater than zero then k-- when j and k crosses or are equal then i++ and
+  //  repeat
+  // Also note to take every element that is taken at an iteration dont repeat
+  // as we dont want to store duplicates Exampo [-2 -2 -2 -1 -1 -1 0 0 0 2 2 2]
+  // So when -2 0 2 are taken taken then we do j++ till j is not equal to zero
+  // and same for k, k-- till k is not 2
+  // Similarly for i too we will move i, i++ till next element is not -2
+
+  vector<vector<int>> ans;
+  sort(arr.begin(), arr.end());
+  for (int i = 0; i < size; i++) {
+    if (i > 0 && arr[i] == arr[i - 1]) {
+      // first element and same element
+      continue;
+    }
+
+    int j = i + 1;
+    int k = size - 1;
+
+    while (j < k) {
+      int sum = arr[i] + arr[j] + arr[k];
+
+      if (sum < 0) {
+        j++;
+      } else if (sum > 0) {
+        k--;
+      } else {
+        // sum = 0
+        vector<int> temp = {arr[i], arr[j], arr[k]};
+        ans.push_back(temp);
+        j++;
+        k--;
+
+        while (j < k && arr[j] == arr[j - 1])
+          j++;
+        while (j < k && arr[k] == arr[k + 1])
+          k--;
+      }
+    }
+  }
+
+  return ans;
+}
 int main() {
   cout << "3 Sum";
   vector<int> arr = {-1, 0, 1, 2, -1, -4};
@@ -63,7 +118,15 @@ int main() {
 
   // Brute
   // with three interations
-  vector<vector<int>> ans = brute3Sum(arr, arr.size());
+  // vector<vector<int>> ans = brute3Sum(arr, arr.size());
+
+  // for (auto row : ans) {
+  //   for (auto it : row) {
+  //     cout << it << " ";
+  //   }
+  //   cout << endl;
+  // }
+  vector<vector<int>> ans = optimal3Sum(arr, arr.size());
 
   for (auto row : ans) {
     for (auto it : row) {
