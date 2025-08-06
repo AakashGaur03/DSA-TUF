2fbdf003a4f00629172107277e5757bf6e3f4b5a
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-08-06 15:30:49 +0530

    4 Sum optimal Approach

--- 8_Array_Hard_Problems/4_4_Sum.cpp
+++ 8_Array_Hard_Problems/4_4_Sum.cpp
@@ -49,6 +49,64 @@ vector<vector<int>> better4Sum(vector<int> arr, int size) {
   return ans;
 }
 
+vector<vector<int>> optimal4Sum(vector<int> arr, int size, int target) {
+  // Same approach as 3 sum optimal
+  // Sort The Array
+  // We will fix two variables and then same logic as in 3 Sum
+
+  // TC O(N^3)
+  // SC O(M) number of quads (space used to return answer)
+
+  // We will use two fixed pointers (i, j) and two moving pointers (k, l)
+  // Sort the array to enable skipping duplicates and two-pointer logic
+
+  vector<vector<int>> ans;
+
+  sort(arr.begin(), arr.end());
+
+  for (int i = 0; i < size; i++) {
+    // Skip duplicate values for the first pointer
+    if (i > 0 && arr[i] == arr[i - 1]) {
+      continue;
+    }
+    for (int j = i + 1; j < size; j++) {
+      // Skip duplicate values for the second pointer
+      if (j != i + 1 && arr[j] == arr[j - 1]) {
+        continue;
+      }
+      int k = j + 1;
+      int l = size - 1;
+      while (k < l) {
+        // Use long long to avoid integer overflow
+        long long sum = arr[i];
+        sum += arr[j];
+        sum += arr[k];
+        sum += arr[l];
+        if (sum == target) {
+          vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+          ans.push_back(temp);
+          k++;
+          l--;
+          // Skip duplicate values for third pointer
+          while (k < l && arr[k] == arr[k - 1]) {
+            k++;
+          }
+          // Skip duplicate values for fourth pointer
+          while (k < l && arr[l] == arr[l + 1]) {
+            l--;
+          }
+        } else if (sum < target) {
+          k++;
+        } else {
+          l--;
+        }
+      }
+    }
+  }
+
+  return ans;
+}
+
 int main() {
   cout << "4 Sum";
   vector<int> arr = {1, 0, -1, 0, -2, 2};
@@ -61,13 +119,14 @@ int main() {
   //   }
   //   cout << endl;
   // }
-  vector<vector<int>> ans = better4Sum(arr, arr.size());
+  // vector<vector<int>> ans = better4Sum(arr, arr.size());
 
-  for (auto row : ans) {
-    for (auto it : row) {
-      cout << it << " ";
-    }
-    cout << endl;
-  }
+  // for (auto row : ans) {
+  //   for (auto it : row) {
+  //     cout << it << " ";
+  //   }
+  //   cout << endl;
+  // }
+
   return 0;
 }
