b2ce3e3c13ee113d74e3f1e9d62a755e0a152d69
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-09-13 01:06:09 +0530

    Traversal Code and Interative Preorder code

--- 10_Trees/3_Traversal_BT.cpp
+++ 10_Trees/3_Traversal_BT.cpp
@@ -1,5 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+struct Node {
+  int data;
+  Node *left;
+  Node *right;
+
+  Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+// Function to perform preorder Traversal
+// of the tree and store values in 'arr'
+void preorder(Node *root, vector<int> &arr) {
+  if (root == nullptr) {
+    return;
+  }
+  arr.push_back(root->data);
+  preorder(root->left, arr);
+  preorder(root->right, arr);
+}
+
+// Fuction to initiate preorder Traversal
+// and return resulting vector
+
+vector<int> preOrder(Node *root) {
+  vector<int> arr;
+
+  preorder(root, arr);
+
+  return arr;
+}
+
+void postorder(Node *root, vector<int> &arr) {
+  if (root == nullptr) {
+    return;
+  }
+  postorder(root->left, arr);
+  postorder(root->right, arr);
+  arr.push_back(root->data);
+}
+
+vector<int> postOrder(Node *root) {
+  vector<int> arr;
+
+  postorder(root, arr);
+
+  return arr;
+}
+
+void inorder(Node *root, vector<int> &arr) {
+  if (root == nullptr) {
+    return;
+  }
+  inorder(root->left, arr);
+  arr.push_back(root->data);
+  inorder(root->right, arr);
+}
+
+vector<int> inOrder(Node *root) {
+  vector<int> arr;
+
+  inorder(root, arr);
+
+  return arr;
+}
+
 int main() {
   cout << "3 Traversal BT";
 
@@ -25,6 +90,24 @@ int main() {
   //     8       9   10
   //   1 2 4 5 8 3 6 7 9 10
 
+  Node *root = new Node(1);
+  root->left = new Node(2);
+  root->right = new Node(3);
+  root->left->left = new Node(4);
+  root->left->right = new Node(5);
+  //   EX
+  //         1
+  //      /     \
+  //     2       3
+  //    / \ 
+  //   4   5
+
+  vector<int> result = preOrder(root);
+  cout << "Preorder Traversal: ";
+  for (int val : result) {
+    cout << val << " ";
+  }
+
   // In Order Traversal (Left Root Right)
   // TC O(N)
   // SC O(Height of the tree) ar worst O(N)
@@ -48,6 +131,13 @@ int main() {
   //     8       9   10
   // 4 2 8 5 1 6 3 9 7 10
 
+  vector<int> result2 = inOrder(root);
+  cout << endl;
+  cout << "inOrder Traversal: ";
+  for (int val2 : result2) {
+    cout << val2 << " ";
+  }
+
   // Post Order Traversal (Left Right Root)
   // TC O(N)
   // SC O(Height of the tree) ar worst O(N)
@@ -61,6 +151,12 @@ int main() {
   //     print(node->data);
   //   }
 
+  vector<int> result3 = postOrder(root);
+  cout << endl;
+  cout << "PostOrder Traversal: ";
+  for (int val3 : result3) {
+    cout << val3 << " ";
+  }
   //   EX
   //         1
   //      /     \
