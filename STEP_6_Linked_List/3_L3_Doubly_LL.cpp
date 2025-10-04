3f4ff997d4f57b9a1ba64c963e87e2633fd23b6e
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-10-05 02:32:44 +0530

    Insertion in Doubly LL

--- STEP_6_Linked_List/3_L3_Doubly_LL.cpp
+++ STEP_6_Linked_List/3_L3_Doubly_LL.cpp
@@ -129,6 +129,62 @@ void deleteNodeDoublyLL(Node *temp) {
   return;
 }
 
+Node *insertBeforeHeadDoublyLL(Node *head, int val) {
+  Node *newHead = new Node(val, head, nullptr);
+  head->back = newHead;
+
+  return newHead;
+}
+Node *insertBeforeTailDoublyLL(Node *head, int val) {
+  if (head->next == NULL) { // Only one element
+    return insertBeforeHeadDoublyLL(head, val);
+  }
+  Node *tail = head;
+  while (tail->next != NULL) {
+    tail = tail->next;
+  }
+  Node *prev = tail->back;
+  Node *newNode = new Node(val, tail, prev);
+  prev->next = newNode;
+  tail->back = newNode;
+  return head;
+}
+
+Node *insertBeforeKthDoublyLL(Node *head, int val, int k) {
+  if (head == NULL) {
+    return new Node(val, NULL, NULL);
+  }
+  if (k == 1) {
+    return insertBeforeHeadDoublyLL(head, val);
+  }
+  Node *temp = head;
+  int cnt = 0;
+  while (temp != NULL) {
+    cnt++;
+    if (cnt == k) {
+      break;
+    }
+    temp = temp->next;
+  }
+  // Case 3: k > length → no insertion
+  if (temp == NULL) {
+    return head;
+  }
+
+  Node *prev = temp->back;
+  Node *newNode = new Node(val, temp, prev);
+  prev->next = newNode;
+  temp->back = newNode;
+  return head;
+}
+
+void insertBeforeNodeDoublyLL(Node *node, int val) {
+  Node *prev = node->back;
+  Node *newNode = new Node(val, node, prev);
+  prev->next = newNode;
+  node->back = newNode;
+}
+
 int main() {
   cout << "3 L3 Doubly LL" << endl;
   // We can go in both directions
@@ -153,6 +209,23 @@ int main() {
   // Delete Node
   // Node will never be Head
   deleteNodeDoublyLL(head->next->next);
+  printLL(head);
+
+  // Insertion Before Head
+  head = insertBeforeHeadDoublyLL(head, 10);
+  printLL(head);
+
+  // Insertion Before Tail
+  head = insertBeforeTailDoublyLL(head, 13);
+  printLL(head);
+
+  // Insertion Before Kth Node
+  head = insertBeforeKthDoublyLL(head, 39, 3);
+  printLL(head);
+
+  // Insertion Before Node
+  // This node will not be Head
+  insertBeforeNodeDoublyLL(head->next->next, 23);
   printLL(head);
   return 0;
 }
