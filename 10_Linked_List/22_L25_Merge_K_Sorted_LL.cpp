5d0904a5443d3806ac2312b74bfac6861e9357ea
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-10-29 00:52:38 +0530

    Merge K sorted Lists OPtimal

--- 10_Linked_List/22_L25_Merge_K_Sorted_LL.cpp
+++ 10_Linked_List/22_L25_Merge_K_Sorted_LL.cpp
@@ -114,22 +114,115 @@ Node *bettermergeKSortedList(vector<Node *> list) {
   return head;
 }
 
+Node *optimalMergeKSortedList(vector<Node *> list) {
+  // TC O(K Log K) + (k* N * 3log k) so approx in power of 2 and log k
+  // SC O(K)
+  // for loop K
+  // pq.add so add operation takes log of size of proiority queue that is log k
+  // While loop till pq is empty
+  // pq will have all the elements of nodes that is K * N
+  // and there are 3 operations inside PQ that is add top and pop so 3* log k
+
+  // Min-heap (priority_queue with smallest element at top)
+  // Each element is a pair: {node->data, node_pointer}
+
+  priority_queue<pair<int, Node *>, vector<pair<int, Node *>>,
+                 greater<pair<int, Node *>>>
+      pq;
+
+  if (list.empty())
+    return nullptr;
+
+  // Insert the head node of all K lists into the min -heap
+  for (int i = 0; i < list.size(); i++) {
+    pq.push({list[i]->data, list[i]});
+  }
+  Node *dummyNode = new Node(-1);
+  Node *temp = dummyNode;
+
+  // Extract the smallest element from the heap, attach it to the result,
+  // and push its next node (if any) into the heap
+  while (!pq.empty()) {
+    // auto p = pq.top();
+    pair<int, Node *> p = pq.top();
+    pq.pop();
+    if (p.second->next) {
+      pq.push({p.second->next->data, p.second->next});
+    }
+    temp->next = p.second;
+    temp = p.second; // temp = temp->next
+  }
+  return dummyNode->next;
+}
+
 int main() {
   cout << "22 L25 Merge K sorted Linked List" << endl;
   vector<int> arr = {1, 2, 3, 4, 5};
   vector<int> arr2 = {1, 3, 3, 6, 11, 15};
   vector<int> arr3 = {1, 5, 6, 23, 67};
-  Node *head = convertArr2LL(arr);
+
+  Node *head1 = convertArr2LL(arr);
   Node *head2 = convertArr2LL(arr2);
   Node *head3 = convertArr2LL(arr3);
-  vector<Node *> list = {head, head2, head3};
-  head = mergeKSortedList(list);
-  printLL(head);
-  head2 = bettermergeKSortedList(list);
-  printLL(head2);
+  vector<Node *> list1 = {head1, head2, head3};
+  Node *res1 = mergeKSortedList(list1);
+  cout << "Naive mergeKSortedList: ";
+  printLL(res1);
+
+  head1 = convertArr2LL(arr);
+  head2 = convertArr2LL(arr2);
+  head3 = convertArr2LL(arr3);
+  vector<Node *> list2 = {head1, head2, head3};
+  Node *res2 = bettermergeKSortedList(list2);
+  cout << "Better mergeKSortedList: ";
+  printLL(res2);
+
+  head1 = convertArr2LL(arr);
+  head2 = convertArr2LL(arr2);
+  head3 = convertArr2LL(arr3);
+  vector<Node *> list3 = {head1, head2, head3};
+  Node *res3 = optimalMergeKSortedList(list3);
+  cout << "Optimal mergeKSortedList: ";
+  printLL(res3);
 
   // For optimal we will be using concept of min-heap
   // min-heap is simply that gives us minimum value
   // in C++ Java we have Priority Queue Data Structure that implements min heap
   return 0;
 }
+
+//   A priority queue is a special queue where:
+// The highest priority element (by default, the largest) is always at the
+// top. But here we’ll modify it to make it a min-heap, so the smallest
+// element comes first.
+
+//   pair<int, Node *>
+// This defines the type of data stored in the priority queue. that is pair
+
+// vector<pair<int, Node *>>
+// This is the underlying container used by the priority queue internally.
+// “Store all pairs inside a std::vector, which will be managed as a heap.”
+
+// greater<pair<int, Node *>>
+// This defines the comparison rule.
+// By default, priority_queue in C++ is a max-heap (largest element at top).
+// So we use greater<...> to make it a min-heap (smallest element at top).
+
+priority_queue<pair<int, Node *>, vector<pair<int, Node *>>,
+               greater<pair<int, Node *>>>
+    pq;
+
+// priority_queue<int> pq;  // MAX-HEAP by default
+
+// So both of below ones are same:
+
+// priority_queue<int> pq;                        // default max heap
+// priority_queue<int, vector<int>, less<int>> pq; // explicit max heap
+
+// Example
+// priority_queue<pair<int, int>> maxHeap; // default = compares by first value
+// (max-heap)
+
+// For min Heap of pairs
+// priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,
+// int>>> minHeap;
