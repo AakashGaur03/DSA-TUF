d6a38b010465a903878b3cef10750bbba222da1a
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-11-07 01:07:37 +0530

    Subsequences

---
+++ 11_Recursion/6_L6_Recursion_on_Subsequences.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "6 L6 Recursion on Subsequences" << endl;
+  // Subsequence means
+  // a contigious/non contigious sequence which follows the order
+
+  // Ex arr=[3,1,2]
+  // {} Subsequence
+  // 3 contigious Subsequence
+  // 1 contigious Subsequence
+  // 2 contigious Subsequence
+  // 3 1 contigious Subsequence
+  // 1 2 contigious Subsequence
+  // 3 2 non - contigious Subsequence
+  // 3 1 2 contigious Subsequence
+  return 0;
+}
