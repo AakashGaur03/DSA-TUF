0bb3255367f99ef74628266efd31ce6d0d0ee0c6
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-12-23 17:38:20 +0530

    Divide Two Numbers without Divide Sign

--- 12_Bit_Manipulation/5_L9_Divide_Two_Integers_without_using_Multiplication_and_Division_Operators.cpp
+++ 12_Bit_Manipulation/5_L9_Divide_Two_Integers_without_using_Multiplication_and_Division_Operators.cpp
@@ -18,6 +18,41 @@ int bruteDivideTwoNumbers(int dividend, int divisor) {
   return cnt - 1;
 }
 
+int optimalDivideTwoNumbers(int dividend, int divisor) {
+
+  // TC: O(log(dividend))
+  // SC: O(1)
+
+  if (dividend == divisor) {
+    return 1;
+  }
+  if (divisor == 0)
+    return INT_MAX;
+
+  bool sign = true;
+  if (dividend >= 0 && divisor < 0)
+    sign = false;
+  else if (dividend <= 0 && divisor > 0)
+    sign = false;
+
+  long n = abs(dividend);
+  long d = abs(divisor);
+  long quotient = 0;
+  while (n >= d) {
+    int cnt = 0;
+    while (n >= (d << (cnt + 1))) { // d*2^(cnt+1)
+      // Find the largest power such that (d << cnt) <= n
+      cnt += 1;
+    }
+    quotient += 1 << cnt; // Add 2^cnt to quotient
+    n -= (d << cnt);      // Subtract d * 2^cnt
+  }
+  if (quotient > INT_MAX)
+    return sign ? INT_MAX : INT_MIN;
+
+  return sign ? quotient : -quotient;
+}
+
 int main() {
   cout << "5 L9 Divide Two Integers without using Multiplication and Division "
           "Operators"
@@ -25,8 +60,10 @@ int main() {
   // we will be given Divided and Divisor we need to divide them
   // without using Division or Multiplication
   int dividend = 22;
-  int dividor = 3;
-  int res = bruteDivideTwoNumbers(dividend, dividor);
+  int divisor = 3;
+  int res = bruteDivideTwoNumbers(dividend, divisor);
   cout << res;
+  int res2 = optimalDivideTwoNumbers(dividend, divisor);
+  cout << res2;
   return 0;
 }
