d1b20ab62239d5008f400b06c2800378fa7398b4
Aakash Gaur <aakash.sunrise23@gmail.com>
2026-04-15 02:54:48 +0530

    Add introductory graph lesson files

---
+++ 18_Graphs/1_G_1_Introduction_to_Graph.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "1 G 1 Introduction to Graph" << endl;
+
+  // Directed Graph and Unidrected Graph
+  // Refer 1_G_1_Introduction_to_Graph_1
+
+  // Nodes or Vertices are connected by Edge/Undirected Edge/Directed Edge
+
+  // In Directed Graph all the edges are directed
+
+  // BT are also be Graphs
+  // 1_G_1_Introduction_to_Graph_3
+  // Cycles in Graph
+  // When a Graph Start from a Node and can traverse Back to it so then it is
+  // called as Undirected Cyclic Graph
+
+  // Path
+  // Contains a lot of nodes and each of them are reachable
+  // Node can not appear twice in the Path
+  // 1_G_1_Introduction_to_Graph_4
+
+  // Degree in Graph
+
+  // Degree is number of Edges that go at a Node (Undirected Graph)
+  // 1_G_1_Introduction_to_Graph_5
+  // NOTE : Total Number of Degrees in a Graph is 2 * Number of Edges
+
+  // Degree is number of Edges that go at a Node (Directed Graph)
+  // We will have InDegree of Node and OutDegree of Node
+  // 1_G_1_Introduction_to_Graph_6
+  // NOTE : Total Number of Degrees in a Graph is Number of Edges
+
+  // Edge Weight
+  //  Every Edge has some weight
+  // If no weight is assigned to Edge we assume Unit Weight that is 1
+  // 1_G_1_Introduction_to_Graph_7
+  return 0;
+}
