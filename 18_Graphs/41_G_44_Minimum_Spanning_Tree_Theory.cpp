3a8a1c52314b30e65a16fee6dcfc12593bf51072
Aakash Gaur <aakash.sunrise23@gmail.com>
2026-05-23 01:32:24 +0530

    Add city-threshold solver and MST theory

---
+++ 18_Graphs/41_G_44_Minimum_Spanning_Tree_Theory.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  cout << "41_G_44_Minimum_Spanning_Tree_Theory" << endl;
+
+  // Minimum Spanning Tree (MST)
+
+  // First, what is a Spanning Tree?
+  // A spanning tree is a tree formed from a graph such that:
+  // 1. It contains all N nodes
+  // 2. It has exactly N - 1 edges
+  // 3. All nodes are connected/reachable from each other
+  // 4. It does not contain any cycle
+
+  // Refer:
+  // 41_G_44_Minimum_Spanning_Tree_Theory_1
+
+  // From a graph, we can form multiple spanning trees.
+  // Among all possible spanning trees, the one having
+  // the minimum total edge weight is called the
+  // Minimum Spanning Tree (MST).
+
+  // Refer:
+  // 41_G_44_Minimum_Spanning_Tree_Theory_2
+
+  return 0;
+}
