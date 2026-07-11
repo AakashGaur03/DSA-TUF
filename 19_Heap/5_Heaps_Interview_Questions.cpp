6042907dcb0906d022a01493846104e3400d5f27
Aakash Gaur <aakash.sunrise23@gmail.com>
2026-07-12 03:45:12 +0530

    Add Merge K Sorted Arrays solution

--- 19_Heap/5_Heaps_Interview_Questions.cpp
+++ 19_Heap/5_Heaps_Interview_Questions.cpp
@@ -59,18 +59,82 @@ int getKthLargestHeap(vector<int> &arr, int k) {
   return mini.top();
 }
 
+class CustomNode {
+  // Node stored inside the min heap.
+  // We need:
+  // data -> current value
+  // i    -> which array this value belongs to
+  // j    -> index of this value inside that array
+public:
+  int data;
+  int i; // row (array number)
+  int j; // column (index in that array)
+  CustomNode(int data, int row, int col) {
+    this->data = data;
+    i = row;
+    j = col;
+  }
+};
+
+class compare {
+  // Comparator for Min Heap.
+  // The smallest data value should come to the top.
+public:
+  bool operator()(CustomNode *a, CustomNode *b) {
+    // Makes this class callable as a function.
+    // Used by priority_queue to compare two nodes.
+    return a->data > b->data;
+  }
+};
+
+vector<int> mergeKSortedArrays(vector<vector<int>> &kArrays, int k) {
+  // TC O(N*K * log K)
+  // SC O(K)
+  priority_queue<CustomNode *, vector<CustomNode *>, compare> minHeap;
+
+  // Step 1:
+  // Push the first element of every array into the heap.
+  // Initially heap contains K elements
+  // O(K log K)
+  for (int i = 0; i < k; i++) {
+    if (!kArrays[i].empty()) {
+      CustomNode *temp = new CustomNode(kArrays[i][0], i, 0);
+      minHeap.push(temp);
+    }
+  }
+  vector<int> ans;
+  // Step 2:
+  // Repeatedly take the smallest element.
+  // O(N*K)
+  while (minHeap.size() > 0) {
+    CustomNode *temp = minHeap.top();
+    ans.push_back(temp->data);
+    minHeap.pop();
+    int row = temp->i;
+    int col = temp->j;
+    // Push the next element from the same array
+    // (if it exists).
+    if (col + 1 < kArrays[row].size()) {
+      CustomNode *next = new CustomNode(kArrays[row][col + 1], row, col + 1);
+      minHeap.push(next);
+    }
+    delete temp; // Avoid memory leak
+  }
+  return ans;
+}
+
 int main() {
 
   cout << "----------------------------------------------" << endl;
   cout << "Q1 Kth Largest Sum Subaaray" << endl;
   // Kth Largest Sum Subaaray
   //   Problem statement
-  // Given an array of integers, find the Kth largest sum subarray For example,
-  // given the array [1, -2, 3, -4, 5] and K = 2, the 2nd largest sum subarray
-  // would be [3, -4, 5], which has a sum of 4.
+  // Given an array of integers, find the Kth largest sum subarray For
+  // example, given the array [1, -2, 3, -4, 5] and K = 2, the 2nd largest sum
+  // subarray would be [3, -4, 5], which has a sum of 4.
 
-  // Please note that a subarray is the sequence of consecutive elements of the
-  // array.
+  // Please note that a subarray is the sequence of consecutive elements of
+  // the array.
 
   // Brute Force
   // Iterate Over Array get all subarrays sum TC O(N*N)
@@ -93,5 +157,37 @@ int main() {
   cout << "Heap Answer        : " << getKthLargestHeap(arr, k) << endl;
   cout << "----------------------------------------------" << endl;
 
+  cout << "Q2 Merge K Sorted Arrays" << endl;
+
+  // Brute Force
+  // Add everything in ans array and sort them (or create a min heap PQ)
+
+  // Optimized Approach
+  // Note we have given all the Arrays are sorted
+  // So we will pick up first element of each Array and will take the minimum
+  // and put in the ans we can take that using Min Heap
+
+  vector<vector<int>> kArrays = {{1, 4, 7}, {2, 5, 8}, {3, 6, 9}};
+
+  int kArraysCount = kArrays.size();
+
+  cout << "Input Arrays:" << endl;
+  for (int i = 0; i < kArraysCount; i++) {
+    cout << "Array " << i + 1 << ": ";
+    for (int x : kArrays[i]) {
+      cout << x << " ";
+    }
+    cout << endl;
+  }
+
+  vector<int> ans = mergeKSortedArrays(kArrays, kArraysCount);
+
+  cout << "Merged Sorted Array: ";
+  for (int x : ans) {
+    cout << x << " ";
+  }
+  cout << endl;
+
+  cout << "----------------------------------------------" << endl;
   return 0;
 }
