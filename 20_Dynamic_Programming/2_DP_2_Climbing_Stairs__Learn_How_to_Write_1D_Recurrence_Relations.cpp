47817726df8f492b95e73aaa9f09d1e4d264f125
Aakash Gaur <aakash.sunrise23@gmail.com>
2026-08-03 01:29:37 +0530

    DP_2_Climbing_Stairs__Learn_How_to_Write_1D_Recurrence_Relations

---
+++ 20_Dynamic_Programming/2_DP_2_Climbing_Stairs__Learn_How_to_Write_1D_Recurrence_Relations.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "2 DP 2 Climbing Stairs | Learn How to Write 1D Recurrence Relations"
+       << endl;
+
+  // You are climbing a staircase. It takes n steps to reach the top.
+  // Each time you can either climb 1 or 2 steps. In how many distinct ways can
+  // you climb to the top?
+
+  // STEPS TO FOLLOW FOR RECURSION RELATION THAT WILL HELP TO SOLVE IN DP
+  // 1) TRY TO REPRESENT THE PROBLEM IN TERMS OF INDEX
+  // 2) DO ALL POSSIBLE STEPS ON THAT INDEX ACC TO PROBLEM STATEMENT
+  // 3) IF QUESTION SAYS
+  // COUNT ALL THE WAYS -> SUM UP ALL THE STUFF
+  // FIND MINIMUM -> GET MINIMUM OF ALL STUFF
+  // FIND MAXIMUM -> GET MAXIMUM OF ALL STUFF
+
+  // SIMILAR QUESTION TO fibonacci Discussed in
+  // 1_DP_1_Introduction_to_Dynamic_Programming__Memoization__Tabulation__Space_Optimization_Techniques
+  return 0;
+}
