01465186a0a355c7fe8c27cbb4593ba0bc5c55c0
Aakash Gaur <aakash.sunrise23@gmail.com>
2026-08-11 09:14:34 +0530

    9_Unique_Paths_2_DP_on_Grid_with_Maze_Obstacles

--- 20_Dynamic_Programming/9_DP_9_Unique_Paths_2_DP_on_Grid_with_Maze_Obstacles.cpp
+++ 20_Dynamic_Programming/9_DP_9_Unique_Paths_2_DP_on_Grid_with_Maze_Obstacles.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int recursionSol(int m, int n) {
+int recursionSol(int m, int n, vector<vector<int>> &obstacleGrid) {
+  if (m >= 0 && n >= 0 && obstacleGrid[m][n] == 1) {
+    return 0;
+  }
   if (m == 0 && n == 0) {
     return 1;
   }
   if (m < 0 || n < 0) {
     return 0;
   }
-  int up = recursionSol(m - 1, n);
-  int left = recursionSol(m, n - 1);
-
+  int up = recursionSol(m - 1, n, obstacleGrid);
+  int left = recursionSol(m, n - 1, obstacleGrid);
   return up + left;
 }
 
-int memoizationSol(int m, int n, vector<vector<int>> &dp) {
+int memoizationSol(int m, int n, vector<vector<int>> &obstacleGrid,
+                   vector<vector<int>> &dp) {
+  if (m >= 0 && n >= 0 && obstacleGrid[m][n] == 1) {
+    return 0;
+  }
   if (m == 0 && n == 0) {
     return 1;
   }
@@ -24,16 +30,21 @@ int memoizationSol(int m, int n, vector<vector<int>> &dp) {
   if (dp[m][n] != -1) {
     return dp[m][n];
   }
-  int up = memoizationSol(m - 1, n, dp);
-  int left = memoizationSol(m, n - 1, dp);
+  int up = memoizationSol(m - 1, n, obstacleGrid, dp);
+  int left = memoizationSol(m, n - 1, obstacleGrid, dp);
 
   dp[m][n] = up + left;
   return dp[m][n];
 }
 
-int tabulationSol(int m, int n, vector<vector<int>> &dp) {
+int tabulationSol(int m, int n, vector<vector<int>> &obstacleGrid,
+                  vector<vector<int>> &dp) {
   for (int i = 0; i < m; i++) {
     for (int j = 0; j < n; j++) {
+      if (obstacleGrid[i][j] == 1) {
+        dp[i][j] = 0;
+        continue;
+      }
       if (i == 0 && j == 0) {
         dp[i][j] = 1;
       } else {
@@ -51,11 +62,17 @@ int tabulationSol(int m, int n, vector<vector<int>> &dp) {
   }
   return dp[m - 1][n - 1];
 }
-int spaceOptimizedSol(int m, int n, vector<int> &dp) {
+
+int spaceOptimizedSol(int m, int n, vector<vector<int>> &obstacleGrid,
+                      vector<int> &dp) {
 
   for (int i = 0; i < m; i++) {
     vector<int> curr(n, 0);
     for (int j = 0; j < n; j++) {
+      if (obstacleGrid[i][j] == 1) {
+        curr[j] = 0;
+        continue;
+      }
       if (i == 0 && j == 0) {
         curr[j] = 1;
       } else {
@@ -82,24 +99,26 @@ int spaceOptimizedSol(int m, int n, vector<int> &dp) {
 // | Tabulation      |      O(m*n)  |           O(m*n)  |
 // | Space Optimized |      O(m*n)  |             O(n)  |
 
-int uniquePaths(int m, int n) {
+int uniquePaths(vector<vector<int>> &obstacleGrid) {
+  int m = obstacleGrid.size();
+  int n = obstacleGrid[0].size();
   // 1. Recursion
-  int ans1 = recursionSol(m - 1, n - 1);
+  int ans1 = recursionSol(m - 1, n - 1, obstacleGrid);
 
   // 2. Memoization
   vector<vector<int>> dp(m, vector<int>(n, -1));
 
-  int ans2 = memoizationSol(m - 1, n - 1, dp);
+  int ans2 = memoizationSol(m - 1, n - 1, obstacleGrid, dp);
 
   // 3. Tabulation
   vector<vector<int>> dp2(m, vector<int>(n, -1));
 
-  int ans3 = tabulationSol(m, n, dp2);
+  int ans3 = tabulationSol(m, n, obstacleGrid, dp2);
 
   // 4. Space Optimization
   vector<int> dp3(n, 0);
 
-  int ans4 = spaceOptimizedSol(m, n, dp3);
+  int ans4 = spaceOptimizedSol(m, n, obstacleGrid, dp3);
 
   cout << "Recursion: " << ans1 << endl;
   cout << "Memoization: " << ans2 << endl;
@@ -110,7 +129,9 @@ int uniquePaths(int m, int n) {
 }
 
 int main() {
-  cout << "9 DP 9 Unique Paths 2 | DP on Grid with Maze Obstacles" << endl;
+  cout << "9 DP 9 Unique Paths 2 | DP on Grid with Maze Obstacles | Unique "
+          "Paths II"
+       << endl;
   // DP on Grids/2D Matrix
   // Count Paths
   // Count Paths with obstacles
@@ -119,24 +140,24 @@ int main() {
   // Triangle
   // 2 Start Points
 
-  // Question 1
+  // You are given an m x n integer array grid. There is a robot initially
+  // located at the top-left corner (i.e., grid[0][0]). The robot tries to move
+  // to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only
+  // move either down or right at any point in time.
 
-  // Unique Paths
-  // There is a robot on an m x n grid. The robot is initially located at the
-  // top-left corner (i.e., grid[0][0]). The robot tries to move to the
-  // bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move
-  // either down or right at any point in time.
+  // An obstacle and space are marked as 1 or 0 respectively in grid. A path
+  // that the robot takes cannot include any square that is an obstacle.
 
-  // Given the two integers m and n, return the number of possible unique paths
-  // that the robot can take to reach the bottom-right corner.
+  // Return the number of possible unique paths that the robot can take to reach
+  // the bottom-right corner.
 
-  // The test cases are generated so that the answer will be less than or equal
+  // The testcases are generated so that the answer will be less than or equal
   // to 2 * 109.
 
   int m = 3;
   int n = 7;
-
-  cout << "Answer: " << uniquePaths(m, n) << endl;
+  vector<vector<int>> obstacleGrid = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+  cout << "Answer: " << uniquePaths(obstacleGrid) << endl;
 
   return 0;
 }
